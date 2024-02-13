494ef640e770430e0efcc3237863fc293a4ffc84
mateus sant'ana <mateusfdev@outlook.com>
2024-02-12 21:46:21 -0300

    create new table in prisma 🥇🥇🥇

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,26 @@
 import Fastify from 'fastify'
 
 const app = Fastify()
+
 app.get('/', () => {
   return 'hello world'
 })
 
 app.listen({
   port: 8787,
+}).then(() => {
+  console.log('server running ✔✔✔')
 })
+
+
+
+
+
+/**
+ * metodos http: get post put patch e delete
+ * get: semmpre que for buscar info uma rota que vai buscar dados 
+ * post: uma p/ rota criar algo
+ * put: uma rota p/ atualiar algum recurso por completo
+ * patch: rota p/ atualizar algum recurso em especifico
+ * delete: rota para deletar algum recurso
+ */
