c0b18e84e57505c646695c3d01f04af9627da4a8
mateus sant'ana <mateusfdev@outlook.com>
2024-02-12 21:52:58 -0300

    return habits

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,11 @@ import { PrismaClient } from '@prisma/client'
 const app = Fastify()
 const prisma = new PrismaClient()
 
-app.get('/', () => {
-  return 'hello world'
+app.get('/hello', async () => {
+  const habits = await prisma.habit.findMany()
+
+
+  return habits
 })
 
 app.listen({
