76b1f69ae27826ff430bd89ff8b25d486a0f21d5
mateus sant'ana <mateusfdev@outlook.com>
2024-02-12 22:02:23 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import Fastify from 'fastify'
+import cors from "@fastify/cors"
 import { PrismaClient } from '@prisma/client'
 
 const app = Fastify()
 const prisma = new PrismaClient()
+
+app.register(cors, {
+  origin: ['http://localhost:8787']
+})
 
 app.get('/hello', async () => {
   const habits = await prisma.habit.findMany()
